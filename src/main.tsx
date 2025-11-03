ad151440f70734369702b2cf0e2ed7d8d9ac0022
Rudranarayanswain <ruudranarayan985@gmail.com>
2025-11-03 20:52:23 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// ✅ Import Google OAuth provider
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// ✅ Your Google Client ID
+const clientId =
+  "736905272101-bfolp8smrdkl2eg59ss9n5oihcb5ph9n.apps.googleusercontent.com";
+
+createRoot(document.getElementById("root")!).render(
+  <GoogleOAuthProvider clientId={clientId}>
+    <App />
+  </GoogleOAuthProvider>
+);
