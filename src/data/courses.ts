0ddded0aa2f8ec8249481b5be563d056ffa6f7ce
Rudranarayanswain <ruudranarayan985@gmail.com>
2025-11-20 20:04:08 +0530

    Updated courses.ts

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -37,9 +37,7 @@ export interface Testimonial {
 // ---------------------
 // 🧰 Tool Model
 // ---------------------
-export interface Tool {
-  name: string;
-  icon: 
+
 }
 
 // ---------------------
