66f1da2b456f1229143155d08a96536b30eba971
Rudranarayanswain <ruudranarayan985@gmail.com>
2025-11-24 22:33:27 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import "./index.css";
 
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId="736905272101-tgriqpdo39bp5f2js930mb0pj9kvrpik.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
