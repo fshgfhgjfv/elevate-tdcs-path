354f1369e052a4850a59be90f30734e4144d082d
Rudranarayanswain <ruudranarayan985@gmail.com>
2025-11-24 21:51:08 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="736905272101-tgriqpdo39bp5f2js930mb0pj9kvrpik.apps.googleusercontent.com">
       <BrowserRouter>
-        <App />
       </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>
