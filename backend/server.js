0ffbb47996b49d15e70d0476bdff7358c3932bab
Rudranarayanswain <ruudranarayan985@gmail.com>
2025-12-04 21:19:00 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
-
 const app = express();
-app.use(express.json());
+
+// Middleware
 app.use(cors());
+app.use(express.json());
 
-// Connect to MongoDB
+// MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, { dbName: "TDCS_WebApp" })
-  .then(() => console.log("MongoDB Connected Successfully!"))
-  .catch((err) => console.log("Mongo Error: ", err));
+  .connect(process.env.MONGO_URI, { dbName: "TDCS_Web" })
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((err) => console.log("MongoDB Error:", err));
 
-// Simple test API
+// Default Route
 app.get("/", (req, res) => {
-  res.send("Backend running successfully!");
+  res.send("Backend Server Running...");
 });
 
+// Server Port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
