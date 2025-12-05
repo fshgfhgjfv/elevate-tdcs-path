0d1a6953812b0da0d24f01b9d40681eb0e52dc59
Rudranarayanswain <ruudranarayan985@gmail.com>
2025-12-05 19:40:35 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,9 @@ const PORT = process.env.PORT || 5000;
 
 // CORS Setup
 const allowedOrigins = [
-  "http://localhost:5173",       // React dev server
-  "https://www.tdcstechnologies.com", // live website
+  "http://localhost:5173",
+  "http://localhost:5000",       // add this
+  "https://www.tdcstechnologies.com",
 ];
 
 app.use(
