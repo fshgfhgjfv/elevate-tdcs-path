18305fe968c87e97d4957529e719b03c09a3d5fd
Rudranarayanswain <ruudranarayan985@gmail.com>
2025-12-05 21:54:41 +0530

    Updated ContactModel.js

---
+++ backend/schemas/ContactModel.js
@@ -0,0 +1,11 @@
+const { Schema } = require("mongoose");
+
+const ContactSchema = new Schema({
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
+  message: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = { ContactSchema };
