007d742d25312ddedba42a9d9fb15f8f0e7bf88c
Rudranarayanswain <ruudranarayan985@gmail.com>
2025-12-05 21:03:14 +0530

    Deleted Newsletter.js

--- backend/models/Newsletter.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const NewsletterSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  subscribedAt: { type: Date, default: Date.now },
-  active: { type: Boolean, default: true }
-});
-
-export default mongoose.model("Newsletter", NewsletterSchema);
