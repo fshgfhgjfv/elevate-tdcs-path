f526712d0f707023142f527332964f20ba909aea
Rudranarayanswain <ruudranarayan985@gmail.com>
2026-01-02 21:00:48 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// 👇 ADD THIS LINE
+console.log("SUPABASE KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 createRoot(document.getElementById("root")!).render(<App />);
