f22aa25bd181d683b317cf22e611f169ce0f0d8b
Rudranarayanswain <ruudranarayan985@gmail.com>
2026-01-04 20:20:32 +0530

    Updated courses.ts

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -132,7 +132,7 @@ export const courses: Course[] = [
     title: "Bug Hunting & Penetration Testing",
     description:
       "Specialized course covering bug bounty hunting, web security, API testing & professional pentesting.",
-    price: 6999,
+    price: 9999,
     thumbnail:
       "https://blogger.googleusercontent.com/img/a/AVvXsEiDzw2wArcdtq3c1v02jDShldtvfyD2qvWJM2yaXc29klQ_ExVwMldeZrakMyLyyNcCF7nYsNdUxVVBCypNx3TPvxEFgsf74dPzE2DrU2Wx658znAPE_f4ikvGr2M4-sJY_4TmL7QdE7XSgBkD1tn65aeBOrYDZSDa54MK1u5HcANKoBXCsXvEOhLjYCUq3",
     category: "Live Online",
