730c5969899b6f72a9651f83dc1b822485b6a824
Rudranarayanswain <ruudranarayan985@gmail.com>
2026-01-13 22:58:55 +0530

    Updated hardwareProducts.ts

--- src/data/hardwareProducts.ts
+++ src/data/hardwareProducts.ts
@@ -542,7 +542,7 @@ export const hardwareProducts: HardwareProduct[] = [
     id: "tdcs-usb-killer",
     name: "TDCS USB Surge Tester Pro",
     images: [
-      "https://images.unsplash.com/photo-1562408590-e32931084e23?auto=format&fit=crop&q=80&w=600"
+      "https://blogger.googleusercontent.com/img/a/AVvXsEjXWC81FcRGOHzlnaMitnIQ3Y9fnk-X-Wi2EcSikpFOnkIdZHbHriBWvPytKgofh9lJSX1No39vBwjdJz0X2d4c_gB-xWnPnMg7ajfkFE_VMk-sskSt-bn6HD8trkPoaBW38rwoa0uDAQHXtf_nQUdi-plYUlpVipX079W_RBqCSRGYhQ_9d2avdhnPT2W_"
     ],
     category: "Hardware / Destructive",
     rating: 4.9,
